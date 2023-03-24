346bf9fdf4eb4eaeca428ffc95b3f51da93fec14
sunaks <sunakshithapar@gmail.com>
2023-03-23 21:56:47 -0400

    Adding home page view

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,54 @@
-const router = require("express").Router();
+const router = require('express').Router()
 
-router.get("/", (req, res) => {
-  res.send("GET /places");
-});
+router.get('/', (req, res) => {
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: '/images/rest-rant-1.jpg'
+      }, {
+          name: 'Coding Cat Cafe',
+          city: 'Phoenix',
+          state: 'AZ',
+          cuisines: 'Coffee, Bakery',
+          pic: '/images/rest-rant-2.jpg'
+      }]
+      
+      
+    res.render('places/index', {places})
+})
 
-module.exports = router;
+router.get('/new', (req, res) => {
+    res.send('New form for a place')
+})
+
+router.post('/', (req, res) => {
+    res.send('Create a new place')
+})
+
+router.get('/:id', (req, res) => {
+    res.send('Show one place in detail (Associated rants, new rant form, delete rant button)')
+})
+
+router.get('/:id/edit', (req, res) => {
+    res.send('Edit form for a place')
+})
+
+router.put('/:id', (req, res) => {
+    res.send('Make changes to existing place')
+})
+
+router.delete('/:id', (req, res) => {
+    res.send('Delete a place')
+})
+
+router.post('/:id/rant', (req, res) => {
+    res.send('Add rant to a place')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('Delete a rant')
+})
+
+module.exports = router
