cdcb79c20533f80cd7d1293020139d426e755932
sunaks <sunakshithapar@gmail.com>
2023-04-21 14:39:52 -0400

    Done tags

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,10 +2,27 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
-    return (
-        <Def>
-          <main>
-            <h1>{data.place.id}</h1>
+  return (
+  <Def>
+  <main>
+    <div className="row"/>
+      <div className="col-sm-6">
+        <img src={data.place.pic} alt={data.place.name} />
+        <h3>
+          Located in {data.place.city}, {data.place.state}
+        </h3>
+      </div>
+      <div className="col-sm-6">
+        <h2>
+          Description
+        </h2>
+        <h3>
+          {data.place.showEstablished()}
+        </h3>
+        <h4>
+          Serving {data.place.cuisines}
+        </h4>
+        <h1>{data.place.id}</h1>
             <img src = {data.place.pic} alt={data.place.name}/>
             <h1>Currently Unrated</h1>
             <a href={`/places/${data.place.id}/edit`} className="btn btn-warning"> 
@@ -16,8 +33,10 @@ function show (data) {
     Delete
   </button>
 </form> 
+      </div>
           </main>
         </Def>
     )
-}
+  }
+
 module.exports = show
