9bffa9f65c51ec9d4d649390d20a1caed2fb0e07
sunaks <sunakshithapar@gmail.com>
2023-04-22 02:31:20 -0400

    Round method done

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -13,9 +13,18 @@ function show (data) {
     </h3>
   )
   if (data.place.comments.length) {
-      comments = data.place.comments.map(c => {
+      let sumRatings = data.place.comments.reduce((tot, c) => {
+        return tot + c.stars
+      }, 0)
+      let averageRating = sumRatings / data.place.comments.length
+      rating = (
+        <h3>
+          {Math.round(averageRating)} stars
+        </h3>
+      )
+    comments = data.place.comments.map(c => {
         return (
-          <div className="border">
+          <div className="border col-sm-4">
             <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
             <h4>{c.content}</h4>
             <h3>
@@ -26,6 +35,7 @@ function show (data) {
         )
       })
     }
+    
  return (
   <Def>
   <main>
@@ -37,10 +47,12 @@ function show (data) {
         </h3>
       </div>
       <div className="col-sm-6">
-      <h1>{data.place.name}</h1>
+      <h1>{ data.place.name }</h1>
           <h2>
           Rating
           </h2>
+          {rating}
+          <br />
         <h2>
           Description
         </h2>
